190b35a9f0759d3afdcf9f008269b759b29d65f6
emirkovacevic <emir.kovacevic@jjryan.com.au>
2022-01-08 20:49:58 +1000

    test against exception

---
+++ DemoLibraryTests.Tests/ExamplesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DemoLibrary;
+using System.IO;
+
+namespace DemoLibraryTests.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("this is a valid file name.");
+
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldWork()
+        {
+            Assert.Throws<ArgumentException>(() => Examples.ExampleLoadTextFile(""));
+        }
+    }
+}
