f61a6b1c0bacdbcef927b06a312d7ae2a0d58fdd
emirkovacevic <emir.kovacevic@jjryan.com.au>
2022-01-08 20:27:03 +1000

    adding TestProject, and first simple test

---
+++ DemoLibraryTests.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibraryTests.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(4, 3, 7)]
+        [InlineData(21, 5.25, 26.25)]
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
